6e372c4746742e1b16f7eedf0b251e3d526e8f07
Alex Krasnov <alex@i-tools.ru>
2022-08-17 11:37:42 +0500

    Base class for admin crud controllers

---
+++ src/Controller/Admin/BaseCrudController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class BaseCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return '';
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return parent::configureCrud($crud)
+            ->setPageTitle(Crud::PAGE_INDEX, 'Просмотр')
+            ->setPageTitle(Crud::PAGE_NEW, 'Создание')
+            ->setPageTitle(Crud::PAGE_EDIT, 'Редактирование')
+            ->setFormThemes([
+                '@EasyAdmin/crud/form_theme.html.twig',
+                '@FOSCKEditor/Form/ckeditor_widget.html.twig',
+            ])
+            ;
+    }
+}
