caf92fc38594e76e8af62b090f2a55ce993a7278
Alex Krasnov <alex@i-tools.ru>
2022-08-17 12:01:10 +0500

    Added using CKEditorField

---
+++ src/Controller/Admin/PlanetCrudController.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Entity\Planet;
+use App\Form\Field\CKEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use function Symfony\Component\Translation\t;
+
+class PlanetCrudController extends BaseCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Planet::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        $published = BooleanField::new('published', t('Published', [], 'admin.planets'));
+        $createdAt = DateField::new('createdAt', 'Created');
+        $updatedAt = DateField::new('updatedAt', 'Updated');
+        $title = TextField::new('title', t('Title', [], 'admin.planets'));
+        $slug = SlugField::new('slug', t('Slug', [], 'admin.traders'))
+            ->setTargetFieldName('title')
+        ;
+        $description = CKEditorField::new('description', t('Description', [], 'admin.planets'))
+            ->setFormTypeOption('config_name', 'description_config')
+        ;
+        $content = CKEditorField::new('content', t('Content', [], 'admin.planets'));
+
+        return match ($pageName) {
+            Crud::PAGE_EDIT, Crud::PAGE_NEW => [
+                FormField::addTab(t('Basic', [], 'admin.planets')),
+                $published,
+                $title->setColumns(6),
+                $slug->setColumns(6),
+                $description->setColumns(12),
+                $content->setColumns(12),
+                FormField::addTab(t('SEO', [], 'admin.planets')),
+            ],
+            default => [$title, $published, $createdAt, $updatedAt],
+        };
+    }
+}
