6f463dd6294090708646d1062609bb16d086fca1
Alex Krasnov <alex@i-tools.ru>
2022-08-17 14:56:22 +0500

    Update methods

--- src/Interfaces/PlanetInterface.php
+++ src/Interfaces/PlanetInterface.php
@@ -4,10 +4,20 @@ declare(strict_types=1);
 
 namespace App\Interfaces;
 
+use Doctrine\Common\Collections\Collection;
+
 interface PlanetInterface
 {
     public function getId(): ?int;
     public function isPublished(): bool;
+    public function getStones(): Collection;
+    public function setStones(Collection $stones): PlanetInterface;
+    public function addStone(StoneInterface ...$stones): PlanetInterface;
+    public function removeStone(StoneInterface $stone): PlanetInterface;
+    public function getAromas(): Collection;
+    public function setAromas(Collection $aromas): PlanetInterface;
+    public function addAroma(AromaInterface ...$aromas): PlanetInterface;
+    public function removeAroma(AromaInterface $aroma): PlanetInterface;
     public function setPublished(bool $published): void;
     public function getTitle(): ?string;
     public function setTitle(string $title): PlanetInterface;
