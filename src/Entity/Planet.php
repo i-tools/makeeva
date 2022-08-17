a391fdc4511b344ab18a95df61f02ae19ee37117
Alex Krasnov <alex@i-tools.ru>
2022-08-17 10:51:31 +0500

    Entity for planets

---
+++ src/Entity/Planet.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Interfaces\PlanetInterface;
+use App\Repository\PlanetRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
+
+#[ORM\Table(name: 'planets')]
+#[ORM\Index(columns: ['slug'], name: 'idx_planets_slug')]
+#[ORM\Entity(repositoryClass: PlanetRepository::class)]
+class Planet implements PlanetInterface
+{
+    use TimestampableTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $content = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): PlanetInterface
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): PlanetInterface
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): PlanetInterface
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): PlanetInterface
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
