9d9c0b2d49cf407d3df2e2d97c889977b5a00a8f
Alex Krasnov <alex@i-tools.ru>
2022-08-17 15:24:05 +0500

    Update methods

--- src/Interfaces/PlanetInterface.php
+++ src/Interfaces/PlanetInterface.php
@@ -9,7 +9,10 @@ use Doctrine\Common\Collections\Collection;
 interface PlanetInterface
 {
     public function getId(): ?int;
-    public function isPublished(): bool;
+    public function getImageName(): ?string;
+    public function setImageName(?string $imageName): PlanetInterface;
+    public function getDescription(): ?string;
+    public function setDescription(string $description): PlanetInterface;
     public function getStones(): Collection;
     public function setStones(Collection $stones): PlanetInterface;
     public function addStone(StoneInterface ...$stones): PlanetInterface;
@@ -18,13 +21,4 @@ interface PlanetInterface
     public function setAromas(Collection $aromas): PlanetInterface;
     public function addAroma(AromaInterface ...$aromas): PlanetInterface;
     public function removeAroma(AromaInterface $aroma): PlanetInterface;
-    public function setPublished(bool $published): void;
-    public function getTitle(): ?string;
-    public function setTitle(string $title): PlanetInterface;
-    public function getDescription(): ?string;
-    public function setDescription(string $description): PlanetInterface;
-    public function getContent(): ?string;
-    public function setContent(?string $content): PlanetInterface;
-    public function getSlug(): ?string;
-    public function setSlug(string $slug): PlanetInterface;
 }
