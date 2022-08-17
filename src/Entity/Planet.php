0bc538e7997b6328031ff346c5072144de209d35
Alex Krasnov <alex@i-tools.ru>
2022-08-17 14:06:23 +0500

    Added mapping to sones

--- src/Entity/Planet.php
+++ src/Entity/Planet.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use App\Interfaces\PlanetInterface;
+use App\Interfaces\StoneInterface;
 use App\Repository\PlanetRepository;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
@@ -27,6 +29,9 @@ class Planet implements PlanetInterface, TimestampableInterface
     #[ORM\Column(type: 'boolean')]
     private bool $published;
 
+    #[ORM\OneToMany(mappedBy: 'planet', targetEntity: Stone::class, cascade: ['persist'], fetch: 'EXTRA_LAZY')]
+    private Collection $stones;
+
     #[ORM\Column(length: 10)]
     private ?string $title = null;
 
@@ -42,6 +47,68 @@ class Planet implements PlanetInterface, TimestampableInterface
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPublished(): bool
+    {
+        return $this->published;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getStones(): Collection
+    {
+        return $this->stones;
+    }
+
+    /**
+     * @param Collection $stones
+     */
+    public function setStones(Collection $stones): void
+    {
+        $this->stones = $stones;
+    }
+
+    /**
+     * @param StoneInterface ...$stones
+     * @return PlanetInterface
+     */
+    public function addStone(StoneInterface ...$stones): PlanetInterface
+    {
+        foreach ($stones as $stone) {
+            if (!$this->stones->contains($stone)) {
+                $this->stones->add($stone);
+                $stone->setPlanet($this);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param StoneInterface $stone
+     * @return PlanetInterface
+     */
+    public function removeQuest(StoneInterface $stone): PlanetInterface
+    {
+        if ($this->stones->contains($stone)) {
+            $this->stones->removeElement($stone);
+            $stone->setPlanet(null);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param bool $published
+     */
+    public function setPublished(bool $published): void
+    {
+        $this->published = $published;
     }
 
     public function getTitle(): ?string
@@ -92,19 +159,8 @@ class Planet implements PlanetInterface, TimestampableInterface
         return $this;
     }
 
-    /**
-     * @return bool
-     */
-    public function isPublished(): bool
+    public function __toString(): string
     {
-        return $this->published;
-    }
-
-    /**
-     * @param bool $published
-     */
-    public function setPublished(bool $published): void
-    {
-        $this->published = $published;
+        return $this->getTitle();
     }
 }
