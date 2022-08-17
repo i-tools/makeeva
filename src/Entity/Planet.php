b3fd5b77ed3704c149e770952ed4e5d81b349697
Alex Krasnov <alex@i-tools.ru>
2022-08-17 15:23:33 +0500

    Refactoring - using HTMPPage trait

--- src/Entity/Planet.php
+++ src/Entity/Planet.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use App\Interfaces\AromaInterface;
+use App\Interfaces\HTMLPageInterface;
 use App\Interfaces\PlanetInterface;
 use App\Interfaces\StoneInterface;
 use App\Repository\PlanetRepository;
+use App\Traits\HTMLPageTrait;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
@@ -18,17 +20,21 @@ use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
 #[ORM\Index(columns: ['slug'], name: 'idx_planets_slug')]
 #[ORM\Entity(repositoryClass: PlanetRepository::class)]
 #[ORM\HasLifecycleCallbacks]
-class Planet implements PlanetInterface, TimestampableInterface
+class Planet implements HTMLPageInterface, PlanetInterface, TimestampableInterface
 {
     use TimestampableTrait;
+    use HTMLPageTrait;
 
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(type: 'boolean')]
-    private bool $published;
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $imageName = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
 
     #[ORM\OneToMany(mappedBy: 'planet', targetEntity: Stone::class, cascade: ['persist'], fetch: 'EXTRA_LAZY')]
     private Collection $stones;
@@ -36,29 +42,40 @@ class Planet implements PlanetInterface, TimestampableInterface
     #[ORM\OneToMany(mappedBy: 'planet', targetEntity: Aroma::class, cascade: ['persist'], fetch: 'EXTRA_LAZY')]
     private Collection $aromas;
 
-    #[ORM\Column(length: 10)]
-    private ?string $title = null;
-
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $description = null;
-
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $content = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $slug = null;
-
     public function getId(): ?int
     {
         return $this->id;
     }
 
     /**
-     * @return bool
+     * @return string|null
      */
-    public function isPublished(): bool
+    public function getImageName(): ?string
     {
-        return $this->published;
+        return $this->imageName;
+    }
+
+    /**
+     * @param string|null $imageName
+     * @return Planet
+     */
+    public function setImageName(?string $imageName): self
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): PlanetInterface
+    {
+        $this->description = $description;
+
+        return $this;
     }
 
     /**
@@ -155,62 +172,6 @@ class Planet implements PlanetInterface, TimestampableInterface
             $this->aromas->removeElement($aroma);
             $aroma->setPlanet(null);
         }
-
-        return $this;
-    }
-
-    /**
-     * @param bool $published
-     */
-    public function setPublished(bool $published): void
-    {
-        $this->published = $published;
-    }
-
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-
-    public function setTitle(string $title): PlanetInterface
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): PlanetInterface
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getContent(): ?string
-    {
-        return $this->content;
-    }
-
-    public function setContent(?string $content): PlanetInterface
-    {
-        $this->content = $content;
-
-        return $this;
-    }
-
-    public function getSlug(): ?string
-    {
-        return $this->slug;
-    }
-
-    public function setSlug(string $slug): PlanetInterface
-    {
-        $this->slug = $slug;
 
         return $this;
     }
--- src/Entity/Stone.php
+++ src/Entity/Stone.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use App\Interfaces\HTMLPageInterface;
 use App\Interfaces\PlanetInterface;
 use App\Interfaces\StoneInterface;
 use App\Repository\StoneRepository;
-use Doctrine\DBAL\Types\Types;
+use App\Traits\HTMLPageTrait;
 use Doctrine\ORM\Mapping as ORM;
 use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
 use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
@@ -15,93 +16,25 @@ use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
 #[ORM\Table(name: 'stones')]
 #[ORM\Index(columns: ['slug'], name: 'idx_stones_slug')]
 #[ORM\Entity(repositoryClass: StoneRepository::class)]
-class Stone implements StoneInterface, TimestampableInterface
+class Stone implements HTMLPageInterface, StoneInterface, TimestampableInterface
 {
     use TimestampableTrait;
+    use HTMLPageTrait;
 
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(type: 'boolean')]
-    private bool $published;
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $imageName = null;
 
     #[ORM\ManyToOne(targetEntity: Planet::class, inversedBy: 'stones')]
     private ?PlanetInterface $planet = null;
 
-    #[ORM\Column(type: 'string', length: 255, nullable: true)]
-    private ?string $imageName = null;
-
-    #[ORM\Column(length: 10)]
-    private ?string $title = null;
-
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $content = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $slug = null;
-
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isPublished(): bool
-    {
-        return $this->published;
-    }
-
-    /**
-     * @param bool $published
-     * @return StoneInterface
-     */
-    public function setPublished(bool $published): StoneInterface
-    {
-        $this->published = $published;
-
-        return $this;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-
-    /**
-     * @param string|null $title
-     * @return StoneInterface
-     */
-    public function setTitle(?string $title): StoneInterface
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    /**
-     * @return PlanetInterface|null
-     */
-    public function getPlanet(): ?PlanetInterface
-    {
-        return $this->planet;
-    }
-
-    /**
-     * @param PlanetInterface|null $planet
-     * @return StoneInterface
-     */
-    public function setPlanet(?PlanetInterface $planet): StoneInterface
-    {
-        $this->planet = $planet;
-
-        return $this;
     }
 
     /**
@@ -124,39 +57,20 @@ class Stone implements StoneInterface, TimestampableInterface
     }
 
     /**
-     * @return string|null
+     * @return PlanetInterface|null
      */
-    public function getContent(): ?string
+    public function getPlanet(): ?PlanetInterface
     {
-        return $this->content;
+        return $this->planet;
     }
 
     /**
-     * @param string|null $content
+     * @param PlanetInterface|null $planet
      * @return StoneInterface
      */
-    public function setContent(?string $content): StoneInterface
+    public function setPlanet(?PlanetInterface $planet): StoneInterface
     {
-        $this->content = $content;
-
-        return $this;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getSlug(): ?string
-    {
-        return $this->slug;
-    }
-
-    /**
-     * @param string|null $slug
-     * @return StoneInterface
-     */
-    public function setSlug(?string $slug): StoneInterface
-    {
-        $this->slug = $slug;
+        $this->planet = $planet;
 
         return $this;
     }
