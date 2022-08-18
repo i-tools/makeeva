202d988bfaa41581b3840221d6b51ad6ef1e094a
Alex Krasnov <alex@i-tools.ru>
2022-08-18 17:21:22 +0500

    Added sections menu

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
+use App\Entity\Aroma;
 use App\Entity\Planet;
+use App\Entity\Section;
 use App\Entity\Stone;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -26,8 +28,8 @@ class DashboardController extends AbstractDashboardController
         return Dashboard::new()
             ->setTitle('Makeeva site')
             ->renderContentMaximized()
-            ->disableUrlSignatures()
-            ->generateRelativeUrls()
+//            ->disableUrlSignatures()
+//            ->generateRelativeUrls()
             ->setTranslationDomain('admin')
             ->disableDarkMode()
         ;
@@ -36,9 +38,18 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud(t('Sections', [], 'admin.menu'), 'fa fa-map', Section::class)
+            ->setController(SectionCrudController::class)
+        ;
         yield MenuItem::section(t('Content', [], 'admin.menu'), '');
-        yield MenuItem::linkToCrud(t('Planets', [], 'admin.menu'), 'fa fa-map', Planet::class)->setController(PlanetCrudController::class);
-        yield MenuItem::linkToCrud(t('Stones', [], 'admin.menu'), 'fa fa-map', Stone::class)->setController(StoneCrudController::class);
-        yield MenuItem::linkToCrud(t('Aromas', [], 'admin.menu'), 'fa fa-map', Aroma::class)->setController(AromaCrudController::class);
+        yield MenuItem::linkToCrud(t('Planets', [], 'admin.menu'), 'fa fa-map', Planet::class)
+            ->setController(PlanetCrudController::class)
+        ;
+        yield MenuItem::linkToCrud(t('Stones', [], 'admin.menu'), 'fa fa-map', Stone::class)
+            ->setController(StoneCrudController::class)
+        ;
+        yield MenuItem::linkToCrud(t('Aromas', [], 'admin.menu'), 'fa fa-map', Aroma::class)
+            ->setController(AromaCrudController::class)
+        ;
     }
 }
