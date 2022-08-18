91693a076a7952f13529d73819b89fef55588dcc
Alex Krasnov <alex@i-tools.ru>
2022-08-18 18:41:03 +0500

    Entity for gallery entity

--- src/Entity/GalleryEntity.php
+++ src/Entity/GalleryEntity.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use App\Interfaces\GalleriedEntityInterface;
-use App\Repository\GalleriedEntityRepository;
+use App\Interfaces\GalleryEntityInterface;
+use App\Repository\GalleryEntityRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: GalleriedEntityRepository::class)]
-class GalleriedEntity implements GalleriedEntityInterface
+#[ORM\Table(name: 'galleries_entities')]
+#[ORM\Entity(repositoryClass: GalleryEntityRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class GalleryEntity implements GalleryEntityInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -22,6 +25,12 @@ class GalleriedEntity implements GalleriedEntityInterface
 
     #[ORM\Column(type: Types::STRING, length: 255, nullable: false)]
     private string $imageName;
+
+    public static function getEntityClass(): string
+    {
+        // By default, the translatable class has the same name but without the "Translation" suffix
+        return static::class;
+    }
 
     public function getId(): ?int
     {
@@ -38,7 +47,7 @@ class GalleriedEntity implements GalleriedEntityInterface
 
     /**
      * @param string|null $title
-     * @return GalleriedEntity
+     * @return GalleryEntity
      */
     public function setTitle(?string $title): self
     {
@@ -57,11 +66,54 @@ class GalleriedEntity implements GalleriedEntityInterface
 
     /**
      * @param string $imageName
-     * @return GalleriedEntity
+     * @return GalleryEntity
      */
     public function setImageName(string $imageName): self
     {
         $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getImageName();
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getParentType(): ?string
+    {
+        return $this->parentType;
+    }
+
+    /**
+     * @param string $parentType
+     * @return GalleryEntity
+     */
+    public function setParentType(string $parentType): self
+    {
+        $this->parentType = $parentType;
+
+        return $this;
+    }
+
+    /**
+     * @return GalleriedEntityInterface
+     */
+    public function getParent(): GalleriedEntityInterface
+    {
+        return $this->parent;
+    }
+
+    /**
+     * @param GalleriedEntityInterface $parent
+     * @return GalleryEntity
+     */
+    public function setParent(GalleriedEntityInterface $parent): self
+    {
+        $this->parent = $parent;
 
         return $this;
     }
