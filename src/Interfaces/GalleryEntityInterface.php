49121d41e9698cfd902c4273e5a2c40d8c0d3333
Alex Krasnov <alex@i-tools.ru>
2022-08-18 18:41:26 +0500

    Interface for GalleryEntity

--- src/Interfaces/GalleryEntityInterface.php
+++ src/Interfaces/GalleryEntityInterface.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace App\Interfaces;
 
-interface GalleriedEntityInterface
+interface GalleryEntityInterface
 {
     public function getId(): ?int;
     public function getTitle(): ?string;
-    public function setTitle(?string $title): GalleriedEntityInterface;
+    public function setTitle(?string $title): GalleryEntityInterface;
     public function getImageName(): string;
-    public function setImageName(string $imageName): GalleriedEntityInterface;
+    public function setImageName(string $imageName): GalleryEntityInterface;
 }
